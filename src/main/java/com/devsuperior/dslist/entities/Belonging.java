53adb17a127f24a8d89fc6d0ca41b96b3ac28c3b
Jean Yuskey <jeanyuskeyk.kozima@gmail.com>
2025-01-21 19:34:06 -0300

    Database seed update

--- src/main/java/com/devsuperior/dslist/entities/Belonging.java
+++ src/main/java/com/devsuperior/dslist/entities/Belonging.java
@@ -13,16 +13,16 @@ public class Belonging {
 	@EmbeddedId
 	private BelongingPK id = new BelongingPK();
 	
-	private Integer positiion;
+	private Integer position;
 	
 	public Belonging() {
 		
 	}
 
-	public Belonging(Game game, GameList list, Integer positiion) {
+	public Belonging(Game game, GameList list, Integer position) {
 		id.setGame(game);
 		id.setList(list);
-		this.positiion = positiion;
+		this.position = position;
 
 	}
 
@@ -34,12 +34,12 @@ public class Belonging {
 		this.id = id;
 	}
 
-	public Integer getPositiion() {
-		return positiion;
+	public Integer getPosition() {
+		return position;
 	}
 
-	public void setPositiion(Integer positiion) {
-		this.positiion = positiion;
+	public void setPosition(Integer position) {
+		this.position = position;
 	}
 
 	@Override
